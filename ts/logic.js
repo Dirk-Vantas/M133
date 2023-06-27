ef5ef3fe168ddb52f4652717836018889df4aa6c
Gideon Watson <gideon.watson@meteoblue.com>
2023-06-27 15:59:27 +0200

    add table call function and finish classlist pull and list population

--- ts/logic.js
+++ ts/logic.js
@@ -1,32 +1,64 @@
 var professionListAPI = "http://sandbox.gibm.ch/berufe.php";
 var classListAPI = "http://sandbox.gibm.ch/klassen.php";
 function loadOptions(URL, element) {
-    fetch(URL)
-        .then(function (response) {
-        if (!response.ok) {
-            throw new Error('Network response was not OK');
-        }
-        return response.json();
-    })
-        .then(function (response) {
-        var options = response
-            .filter(function (item) { return item.beruf_name.trim() !== ''; }) // Filter out empty labels
-            .map(function (item) { return ({
-            value: item.beruf_id,
-            label: item.beruf_name,
-        }); });
-        console.log(options);
-        populateDropdown(options, element);
-        console.log('API Response:', options);
-    })
-        .catch(function (error) {
-        console.log('Error occurred while fetching options:', error);
-    });
+    if (element == 'professionList') {
+        fetch(URL)
+            .then(function (response) {
+            if (!response.ok) {
+                throw new Error('Network response was not OK');
+            }
+            console.log("proffessionlist api call:");
+            return response.json();
+        })
+            .then(function (response) {
+            var options = response
+                .filter(function (item) { return item.beruf_name.trim() !== ''; }) // Filter out empty labels
+                .map(function (item) { return ({
+                value: item.beruf_id,
+                label: item.beruf_name,
+            }); });
+            console.log(options);
+            populateDropdown(options, element);
+            console.log('API Response:', options);
+        })
+            .catch(function (error) {
+            console.log('Error occurred while fetching options:', error);
+        });
+    }
+    if (element == 'classList') {
+        console.log(URL);
+        fetch(URL)
+            .then(function (response) {
+            if (!response.ok) {
+                throw new Error('Network response was not OK');
+            }
+            return response.json();
+        })
+            .then(function (response) {
+            console.log('API Response:', response);
+            var options = response
+                //.filter((item) => item.beruf_name.trim() !== '') // Filter out empty labels
+                .map(function (item) { return ({
+                value: item.klasse_id,
+                label: item.klasse_name,
+            }); });
+            console.log("classList:");
+            console.log(options);
+            populateDropdown(options, element);
+            console.log('API Response:', options);
+        })
+            .catch(function (error) {
+            console.log('Error occurred while fetching options:', error);
+        });
+    }
 }
 function populateDropdown(options, id) {
-    var dropdown = document.getElementById('professionList');
+    var dropdown = document.getElementById(id);
     // Clear existing options
     dropdown.innerHTML = '';
+    if (id == 'classList') {
+        dropdown.disabled = false;
+    }
     // Populate dropdown with new options
     options.forEach(function (option, index) {
         if (option.label.length != 0) {
@@ -41,6 +73,8 @@ function populateDropdown(options, id) {
         }
     });
 }
+function loadTabel(URL, element) {
+}
 function onDOMLoaded(callback) {
     document.addEventListener('DOMContentLoaded', callback);
 }
@@ -48,17 +82,25 @@ function onDOMLoaded(callback) {
 onDOMLoaded(function () {
     /// Event listener for the first dropdown
     var firstDropdown = document.getElementById('professionList');
+    var secondDropdown = document.getElementById('classList');
     firstDropdown.addEventListener('change', function (event) {
         var selectedValue = event.target.value;
         console.log(selectedValue);
+        //prepare request parameters
+        var request = "?beruf_id=".concat(selectedValue);
+        console.log(classListAPI + request);
+        //load classes
+        loadOptions(classListAPI + request, 'classList');
         //returns ID
-        //TODO read second api documentation and load correct data  and use
-        //existing functions to load and display the data
-        //populateSecondDropdown(selectedValue);
-        //loadOptions(classListAPI)
+    });
+    secondDropdown.addEventListener('change', function (event) {
+        var selectedClass = event.target.value;
+        console.log(selectedClass);
+        var request = "?klasse_id=".concat(selectedClass);
+        console.log(classListAPI + request);
+        //load classes
+        loadOptions(classListAPI + request, 'outputTable');
     });
     // Call the loadOptions function to fetch and populate the dropdown with proffession
     loadOptions(professionListAPI, "professionList");
-    console.log("debug:");
-    console.log(document.getElementById('professionList'));
 });
--- ts/logic.ts
+++ ts/logic.ts
@@ -7,14 +7,16 @@ const professionListAPI: string = "http://sandbox.gibm.ch/berufe.php"
 const classListAPI: string = "http://sandbox.gibm.ch/klassen.php"
 
 function loadOptions(URL: string, element: string): void {
+  
+  if(element == 'professionList'){
   fetch(URL)
     .then((response) => {
       if (!response.ok) {
         throw new Error('Network response was not OK');
       }
+      console.log("proffessionlist api call:")
       return response.json();
     })
-    
     .then((response: { beruf_name: string; beruf_id: string }[]) => {
       const options: Option[] = response
       .filter((item) => item.beruf_name.trim() !== '') // Filter out empty labels
@@ -30,14 +32,47 @@ function loadOptions(URL: string, element: string): void {
     .catch((error) => {
       console.log('Error occurred while fetching options:', error);
     });
+  }
+
+  if(element == 'classList'){
+    console.log(URL)
+    fetch(URL)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not OK');
+      }
+      return response.json();
+    })
+    .then((response: { klasse_id: string; klasse_name: string }[]) => {
+      console.log('API Response:', response);
+      const options: Option[] = response
+      //.filter((item) => item.beruf_name.trim() !== '') // Filter out empty labels
+      .map((item) => ({
+        value: item.klasse_id,
+        label: item.klasse_name,
+      }));
+      console.log("classList:");
+      console.log(options);
+      populateDropdown(options, element);
+      console.log('API Response:', options);
+    })
+    .catch((error) => {
+      console.log('Error occurred while fetching options:', error);
+    });
+  }
+
 }
 
 
 function populateDropdown(options: Option[], id:string): void {
-  const dropdown = document.getElementById('professionList') as HTMLSelectElement;
+  const dropdown = document.getElementById(id) as HTMLSelectElement;
 
   // Clear existing options
   dropdown.innerHTML = '';
+
+  if (id == 'classList'){
+    dropdown.disabled = false;
+  }
 
   // Populate dropdown with new options
   options.forEach((option,index) => {
@@ -56,6 +91,12 @@ function populateDropdown(options: Option[], id:string): void {
   });
 }
 
+function loadTabel(URL: string, element: string): void {
+
+
+
+}
+
 
 
 
@@ -67,19 +108,34 @@ function onDOMLoaded(callback: () => void) {
 onDOMLoaded(() => {
   /// Event listener for the first dropdown
   const firstDropdown = document.getElementById('professionList') as HTMLSelectElement;
+  const secondDropdown = document.getElementById('classList') as HTMLSelectElement;
+  
   firstDropdown.addEventListener('change', (event) => {
     const selectedValue = (event.target as HTMLSelectElement).value;
     console.log(selectedValue)
+    //prepare request parameters
+    var request = `?beruf_id=${selectedValue}` 
+    console.log(classListAPI+request)
+    //load classes
+    loadOptions(classListAPI+request,'classList')
     //returns ID
-    //TODO read second api documentation and load correct data  and use
-    //existing functions to load and display the data
-    //populateSecondDropdown(selectedValue);
-    //loadOptions(classListAPI)
   });
+
+  secondDropdown.addEventListener('change', (event) =>{
+    const selectedClass = (event.target as HTMLSelectElement).value;
+    
+    console.log(selectedClass)
+    
+    var request = `?klasse_id=${selectedClass}` 
+    console.log(classListAPI+request)
+    //load classes
+    loadOptions(classListAPI+request,'outputTable')
+
+
+  })
   // Call the loadOptions function to fetch and populate the dropdown with proffession
   loadOptions(professionListAPI,"professionList");
-  console.log("debug:")
-  console.log(document.getElementById('professionList'))
+  
 });
 
 
