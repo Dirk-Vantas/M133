9962d3d3b99a10fc8be3d5350418e30037d52dd7
Gideon Watson <gideon.watson@meteoblue.com>
2023-06-27 17:48:04 +0200

    started work on the datepicker inclusion, this is a gigantic headache.

--- ts/logic.js
+++ ts/logic.js
@@ -1,5 +1,9 @@
 var professionListAPI = "http://sandbox.gibm.ch/berufe.php";
-var classListAPI = "http://sandbox.gibm.ch/klassen.php";
+var classListAPI = "http://sandbox.gibm.ch/klassen.php?beruf_id=";
+var lessonListAPI = "http://sandbox.gibm.ch/tafel.php?klasse_id=";
+/* REQUIRED PACKAGES
+- date-fns
+*/
 function loadOptions(URL, element) {
     if (element == 'professionList') {
         fetch(URL)
@@ -37,7 +41,6 @@ function loadOptions(URL, element) {
             .then(function (response) {
             console.log('API Response:', response);
             var options = response
-                //.filter((item) => item.beruf_name.trim() !== '') // Filter out empty labels
                 .map(function (item) { return ({
                 value: item.klasse_id,
                 label: item.klasse_name,
@@ -73,7 +76,61 @@ function populateDropdown(options, id) {
         }
     });
 }
-function loadTabel(URL, element) {
+function loadTable(URL, element) {
+    fetch(URL)
+        .then(function (response) {
+        if (!response.ok) {
+            throw new Error('Network response was not OK');
+        }
+        return response.json();
+    })
+        .then(function (response) {
+        console.log('repsonse classes:', response);
+        //nicely map them into the interface
+        var scheduleItems = response
+            .map(function (item) { return ({
+            id: item.tafel_id,
+            datum: item.tafel_datum,
+            wochentag: item.tafel_wochentag,
+            von: item.tafel_von,
+            bis: item.tafel_bis,
+            lehrer: item.tafel_lehrer,
+            fach: item.tafel_fach,
+            longfach: item.tafel_longfach,
+            raum: item.tafel_raum,
+            kommentar: item.tafel_kommentar,
+        }); });
+        console.log("items:");
+        console.log(scheduleItems);
+        var datePicker = document.getElementById('datepicker');
+        filterTableByWeek(datePicker.value, scheduleItems);
+        //console.log('API Response:', options);
+    })
+        .catch(function (error) {
+        console.log('Error occurred while fetching options:', error);
+    });
+}
+function filterTableByWeek(date, scheduleItems) {
+    console.log(date);
+    //FORMAT is JJJJ-W23
+    // Get the selected date from the date picker
+    var datePicker = document.getElementById('datepicker');
+    var selectedDate = new Date(datePicker.value);
+    console.log(selectedDate);
+    // Get the ISO week number of the selected date
+    var selectedWeek = getWeekNumber(selectedDate);
+    //get all valid entries from the call
+    // Filter the data array based on the week number
+    var filteredData = scheduleItems.filter(function (item) { return getWeekNumber(new Date(item.datum)) === selectedWeek; });
+    console.log('filtered table', filterTableByWeek);
+}
+function getWeekNumber(d) {
+    var currentdate = new Date();
+    var oneJan = new Date(currentdate.getFullYear(), 0, 1);
+    var numberOfDays = Math.floor((currentdate.getTime() - oneJan.getTime()) / (24 * 60 * 60 * 1000));
+    var result = Math.ceil((currentdate.getDay() + 1 + numberOfDays) / 7);
+    console.log("The week number of the current date (".concat(currentdate, ") is ").concat(result, "."));
+    return result;
 }
 function onDOMLoaded(callback) {
     document.addEventListener('DOMContentLoaded', callback);
@@ -83,24 +140,37 @@ onDOMLoaded(function () {
     /// Event listener for the first dropdown
     var firstDropdown = document.getElementById('professionList');
     var secondDropdown = document.getElementById('classList');
+    // Get the date picker element
+    var datePicker = document.getElementById('datepicker');
+    //handle default if no entry has been made
+    if (localStorage.getItem('pickedDate') === null) {
+        var currentDate = new Date();
+        var isoWeekNumber = getWeekNumber(currentDate);
+        console.log('no date set using default, now!');
+        datePicker.value = new Date().toISOString().split('T')[0]; //isoWeekNumber.toString();
+    }
+    else {
+        //datePicker.value = localStorage.getItem('pickedDate');
+    }
     firstDropdown.addEventListener('change', function (event) {
         var selectedValue = event.target.value;
         console.log(selectedValue);
         //prepare request parameters
-        var request = "?beruf_id=".concat(selectedValue);
-        console.log(classListAPI + request);
+        console.log(classListAPI + selectedValue);
         //load classes
-        loadOptions(classListAPI + request, 'classList');
+        loadOptions(classListAPI + selectedValue, 'classList');
         //returns ID
     });
     secondDropdown.addEventListener('change', function (event) {
         var selectedClass = event.target.value;
         console.log(selectedClass);
         var request = "?klasse_id=".concat(selectedClass);
-        console.log(classListAPI + request);
+        console.log(classListAPI + selectedClass);
         //load classes
-        loadOptions(classListAPI + request, 'outputTable');
+        loadTable(lessonListAPI + selectedClass, 'outputTable');
     });
-    // Call the loadOptions function to fetch and populate the dropdown with proffession
+    // Add event listener to detect date changes
+    //datePicker.addEventListener('change', () => filterTableByWeek(datePicker.value, ));
+    // Call the loadOptions function to initialize and fetch and populate the dropdown with profession
     loadOptions(professionListAPI, "professionList");
 });
--- ts/logic.ts
+++ ts/logic.ts
@@ -3,8 +3,27 @@ interface Option {
   label: string;
 }
 
+interface ScheduleItem {
+  id: string;
+  datum: string;
+  wochentag: string;
+  von: string;
+  bis: string;
+  lehrer: string;
+  fach: string;
+  longfach: string;
+  raum: string;
+  kommentar: string;
+}
+
 const professionListAPI: string = "http://sandbox.gibm.ch/berufe.php"
-const classListAPI: string = "http://sandbox.gibm.ch/klassen.php"
+const classListAPI: string = "http://sandbox.gibm.ch/klassen.php?beruf_id="
+const lessonListAPI: string = "http://sandbox.gibm.ch/tafel.php?klasse_id="
+
+/* REQUIRED PACKAGES
+- date-fns
+*/
+
 
 function loadOptions(URL: string, element: string): void {
   
@@ -46,7 +65,6 @@ function loadOptions(URL: string, element: string): void {
     .then((response: { klasse_id: string; klasse_name: string }[]) => {
       console.log('API Response:', response);
       const options: Option[] = response
-      //.filter((item) => item.beruf_name.trim() !== '') // Filter out empty labels
       .map((item) => ({
         value: item.klasse_id,
         label: item.klasse_name,
@@ -62,7 +80,6 @@ function loadOptions(URL: string, element: string): void {
   }
 
 }
-
 
 function populateDropdown(options: Option[], id:string): void {
   const dropdown = document.getElementById(id) as HTMLSelectElement;
@@ -91,13 +108,82 @@ function populateDropdown(options: Option[], id:string): void {
   });
 }
 
-function loadTabel(URL: string, element: string): void {
-
-
-
+function loadTable(URL: string, element: string): void {
+    fetch(URL)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not OK');
+      }
+      return response.json();
+    })
+    .then((response: { 
+      tafel_id: string; 
+      tafel_datum: string; 
+      tafel_wochentag: string;
+      tafel_von: string;
+      tafel_bis: string;
+      tafel_lehrer: string;
+      tafel_fach: string;
+      tafel_longfach: string;
+      tafel_raum:string;
+      tafel_kommentar:string}[]) => {
+        console.log('repsonse classes:',response)
+        //nicely map them into the interface
+        const scheduleItems: ScheduleItem[] = response
+        .map((item) => ({
+          id: item.tafel_id,
+          datum: item.tafel_datum,
+          wochentag: item.tafel_wochentag,
+          von: item.tafel_von,
+          bis: item.tafel_bis,
+          lehrer: item.tafel_lehrer,
+          fach: item.tafel_fach,
+          longfach: item.tafel_longfach,
+          raum: item.tafel_raum,
+          kommentar: item.tafel_kommentar,
+        }));
+        console.log("items:")
+        console.log(scheduleItems);
+        const datePicker = document.getElementById('datepicker') as HTMLInputElement;
+        filterTableByWeek(datePicker.value, scheduleItems);
+        //console.log('API Response:', options);
+      })
+    .catch((error) => {
+      console.log('Error occurred while fetching options:', error);
+    });
 }
 
+function filterTableByWeek(date:string, scheduleItems: ScheduleItem[]) :void{
 
+  console.log(date)
+
+  //FORMAT is JJJJ-W23
+
+  // Get the selected date from the date picker
+  const datePicker = document.getElementById('datepicker') as HTMLInputElement;
+  const selectedDate = new Date(datePicker.value);
+  
+  console.log(selectedDate)
+  // Get the ISO week number of the selected date
+  const selectedWeek = getWeekNumber(selectedDate);
+
+  //get all valid entries from the call
+  // Filter the data array based on the week number
+  const filteredData = scheduleItems.filter((item) => getWeekNumber(new Date(item.datum)) === selectedWeek);
+  
+  console.log('filtered table',filterTableByWeek);
+  
+}
+
+  function getWeekNumber(d: Date): number{
+    const currentdate = new Date();
+    const oneJan = new Date(currentdate.getFullYear(), 0, 1);
+    const numberOfDays = Math.floor((currentdate.getTime() - oneJan.getTime()) / (24 * 60 * 60 * 1000));
+    const result = Math.ceil((currentdate.getDay() + 1 + numberOfDays) / 7);
+    console.log(`The week number of the current date (${currentdate}) is ${result}.`);
+    return result
+  }
+  
 
 
 function onDOMLoaded(callback: () => void) {
@@ -109,15 +195,31 @@ onDOMLoaded(() => {
   /// Event listener for the first dropdown
   const firstDropdown = document.getElementById('professionList') as HTMLSelectElement;
   const secondDropdown = document.getElementById('classList') as HTMLSelectElement;
+
+  // Get the date picker element
+  const datePicker = document.getElementById('datepicker') as HTMLInputElement;
+  
+  //handle default if no entry has been made
+  if (localStorage.getItem('pickedDate') === null){
+    const currentDate = new Date();
+    const isoWeekNumber = getWeekNumber(currentDate)
+    console.log('no date set using default, now!')
+    datePicker.value = new Date().toISOString().split('T')[0];//isoWeekNumber.toString();
+    
+  }
+  else{
+    //datePicker.value = localStorage.getItem('pickedDate');
+  }
+  
   
   firstDropdown.addEventListener('change', (event) => {
     const selectedValue = (event.target as HTMLSelectElement).value;
     console.log(selectedValue)
     //prepare request parameters
-    var request = `?beruf_id=${selectedValue}` 
-    console.log(classListAPI+request)
+    
+    console.log(classListAPI+selectedValue)
     //load classes
-    loadOptions(classListAPI+request,'classList')
+    loadOptions(classListAPI+selectedValue,'classList')
     //returns ID
   });
 
@@ -127,13 +229,16 @@ onDOMLoaded(() => {
     console.log(selectedClass)
     
     var request = `?klasse_id=${selectedClass}` 
-    console.log(classListAPI+request)
+    console.log(classListAPI+selectedClass)
     //load classes
-    loadOptions(classListAPI+request,'outputTable')
-
-
+    loadTable(lessonListAPI+selectedClass,'outputTable')
   })
-  // Call the loadOptions function to fetch and populate the dropdown with proffession
+
+  // Add event listener to detect date changes
+  //datePicker.addEventListener('change', () => filterTableByWeek(datePicker.value, ));
+  
+  
+  // Call the loadOptions function to initialize and fetch and populate the dropdown with profession
   loadOptions(professionListAPI,"professionList");
   
 });
